1e9e401430e55e8e0fa2df128b9f9cbaecbba51f
root <root@ns386549.ip-176-31-244.eu>
2021-05-27 11:20:21 +0000

    change ip adress

--- balance.sh
+++ balance.sh
@@ -1,7 +1,7 @@
 PUBLIC_KEY_HEX=$1
-STATE_ROOT_HASH=$(casper-client get-state-root-hash --node-address http://185.198.72.171:7777 | jq -r '.result | .state_root_hash')
-PURSE_UREF=$(casper-client query-state --node-address http://185.198.72.171:7777 --key "$PUBLIC_KEY_HEX" --state-root-hash "$STATE_ROOT_HASH" | jq -r '.result | .stored_value | .Account | .main_purse')
-BALANCE=$( expr $(casper-client get-balance --node-address http://185.198.72.171:7777 --purse-uref "$PURSE_UREF" --state-root-hash "$STATE_ROOT_HASH" | jq -r '.result | .balance_value') / 1000000000)
+STATE_ROOT_HASH=$(casper-client get-state-root-hash --node-address http://188.165.254.13:7777 | jq -r '.result | .state_root_hash')
+PURSE_UREF=$(casper-client query-state --node-address http://188.165.254.13:7777 --key "$PUBLIC_KEY_HEX" --state-root-hash "$STATE_ROOT_HASH" | jq -r '.result | .stored_value | .Account | .main_purse')
+BALANCE=$( expr $(casper-client get-balance --node-address http://188.165.254.13:7777 --purse-uref "$PURSE_UREF" --state-root-hash "$STATE_ROOT_HASH" | jq -r '.result | .balance_value') / 1000000000)
 
 WHITE='\033[0;37m'
 RED='\033[0;31m'
